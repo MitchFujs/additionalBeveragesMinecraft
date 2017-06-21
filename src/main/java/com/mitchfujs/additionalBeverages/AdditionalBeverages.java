4bc2c808eda02d980ee543732b75c26bf6b2faf8
Mitchell Fujs <mitchell.fujs@gmail.com>
2017-06-21 01:28:45 -0400

    File renaming
    Basic rename

---
+++ src/main/java/com/mitchfujs/additionalBeverages/AdditionalBeverages.java
@@ -0,0 +1,19 @@
+package com.mitchfujs.additionalBeverages;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = AdditionalBeverages.MODID, version = AdditionalBeverages.VERSION)
+public class AdditionalBeverages
+{
+    public static final String MODID = "additionalbeverages";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+       
+    }
+}
